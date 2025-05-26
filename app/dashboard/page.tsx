ae212f8f057d06737dce9af19aa37fff1e7f54ed
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2025-05-25 21:57:03 -0600

    Remove unused imports from dashboard page for cleaner code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import Layout from "@/components/layout/layout";
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
